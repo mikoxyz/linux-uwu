9d9f9fbdb46a14da64133f25b477ce8e531acb5e
Miko <65079739+mikoxyz@users.noreply.github.com>
2021-02-01 09:53:38 +0100

    build.sh: Make the script fit in 80x24

--- build.sh
+++ build.sh
@@ -4,23 +4,26 @@ KERNEL_VER=5.10.12
 
 # Check if git is installed
 if ! [ -x "$(command -v git)" ]; then
-    printf "git is not installed on your system. Please install git using your package manager.\n"
-    exit 1
+	printf "git is either not installed on your system or in your path.\
+Please install git using your package manager or add git to your path.\n"
+	exit 1
 fi
 
-# Run figlet if it's present (I only added this because I wanted to try out git's gpg signing feature; also, it just looks really neat :p)
+# Run figlet if it's present
 if [ -x "$(command -v figlet)" ]; then
-    figlet linux-uwu $KERNEL_VER
+	figlet linux-uwu $KERNEL_VER
 fi
 
-printf "Welcome to the linux-uwu build script!\nDo you want to install the build dependencies? [y/n] "
+printf "Welcome to the linux-uwu build script!\n\
+Do you want to install the build dependencies? [y/n] "
 
 # This is probably really hacky, but it works ¯\_(ツ)_/¯
 while true; do
     read input
     if [ "$input" = "y" ]; then
         printf "Installing the build dependencies..."
-        sudo apt-get update && sudo apt-get install build-essential -y && sudo apt-get build-dep linux -y
+        sudo apt-get update && sudo apt-get install build-essential -y &&\
+        sudo apt-get build-dep linux -y
         break
     elif [ "$input" = "n" ]; then
         break
@@ -32,7 +35,8 @@ done
 echo "Cloning the configs repo..."
 git clone git@github.com:mikoxyz/linux-uwu-configs
 
-# Check if host CPU is Intel and use the Intel config if that's the case. Otherwise, use the normal config
+# Check if host CPU is Intel and use the Intel config if that's the case. 
+# Otherwise, use the normal config
 if lscpu | grep -q Intel; then
     echo "Copying the Intel config to .config..."
     cp linux-uwu-configs/config-${KERNEL_VER}-uwu-intel .config
